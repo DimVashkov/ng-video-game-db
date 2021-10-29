5e89f338aa71a2ab9d92959b266978be1ccb7162
Dimitar Vashkov <dimitarvashkov@outlook.com>
2021-10-29 15:12:11 +0300

    Add game-tabs styling

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-game-tabs',
   templateUrl: './game-tabs.component.html',
-  styleUrls: ['./game-tabs.component.scss']
+  styleUrls: ['./game-tabs.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GameTabsComponent implements OnInit {
 
