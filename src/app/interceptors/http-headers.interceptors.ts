607b8a1d7875aa496bcb3e2b3580ca5bdc48d287
Dimitar Vashkov <dimitarvashkov@outlook.com>
2021-10-29 11:44:01 +0300

    Implemented Http Interceptors

---
+++ src/app/interceptors/http-headers.interceptors.ts
@@ -0,0 +1,23 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor(){}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      'x-rapidapi-key': '8737ca341emshb644138423c712ep134af0jsna5a25e2ee425'
+      },
+      setParams: {
+        key: '6be1bf97b8674ba0aa8e4518c8aec21f',
+      }
+    });
+    return next.handle(req);
+  }
+}
